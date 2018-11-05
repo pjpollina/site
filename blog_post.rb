8effa6e406b5af11f9e00ef01d3aa564cbef9ffa
PJ Pollina <contact@pjpollina.info>
2018-11-05 12:42:04 -0500

    Added erb templating directly to BlogPost class
    - Added constant PAGE_TEMPLATE, loads templates/blog_post.erb
    - Made argument template in function render default to PAGE_TEMPLATE

--- blog_post.rb
+++ blog_post.rb
@@ -1,8 +1,12 @@
 # Class for a blog post object
 # Models data from table defined at schema/posts.sql
 
+require 'erb'
+
 class BlogPost
   attr_reader :title, :date, :body
+
+  PAGE_TEMPLATE = ERB.new(File.read './templates/blog_post.erb')
 
   def initialize(data = {})
     @title  = data['post_title']     || '404 Error'
@@ -10,7 +14,7 @@ class BlogPost
     @body   = data['post_body']      || 'Post not found'
   end
 
-  def render(template)
+  def render(template=PAGE_TEMPLATE)
     template.result(binding)
   end
 end
