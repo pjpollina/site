b4a82b12b4d7fe552f1020fbfb490bcf64882515
PJ Pollina <contact@pjpollina.info>
2018-11-12 16:01:29 -0500

    Added blog_controller.rb

---
+++ blog_controller.rb
@@ -0,0 +1,41 @@
+# Class that controls all blog features of the site
+# ...er, eventually
+
+require 'mysql2'
+require './blog_post.rb'
+require './http_server.rb'
+
+class BlogController
+  def initialize(sql_client: nil)
+    @sql_client = sql_client || Mysql2::Client.new(username: 'blog_server', password: '', database: 'blog')
+  end
+
+  def parse_slug_request(slug)
+    data = stmt_from_slug.execute(slug).first
+    if data.nil?
+      HTTPServer.generic_404
+    else
+      post = BlogPost.new(data)
+      HTTPServer.generic_html(post.render)
+    end
+  end
+
+  private
+
+  def stmt_from_slug
+    @stmt_from_slug ||= @sql_client.prepare <<~SQL
+      SELECT post_title, post_body, post_timestamp
+      FROM posts
+      WHERE post_slug=?
+    SQL
+  end
+
+  def stmt_n_most_recent
+    @stmt_n_most_recent ||= @sql_client.prepare <<~SQL
+      SELECT post_slug, post_title, post_timestamp
+      FROM posts
+      ORDER BY post_timestamp DESC
+      LIMIT ?
+    SQL
+  end
+end
