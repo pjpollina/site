1dba768154f686b7c0210436ca002707833d7b13
PJ Pollina <contact@pjpollina.info>
2018-11-20 16:33:09 -0500

    Slightly modified due to switch to MySQL

--- sql/posts.sql
+++ sql/posts.sql
@@ -1,10 +1,10 @@
 -- SQL Schema for blog posts table
--- Formatted for MariaDB
+-- Formatted for MySQL
 
 CREATE TABLE posts (
   post_id         SMALLINT UNSIGNED   NOT NULL  PRIMARY KEY,  -- The primary key
-  post_title      VARCHAR(190)        NOT NULL  UNIQUE,       -- Title of the post
-  post_slug       VARCHAR(190)        NOT NULL  UNIQUE,       -- URL-safe version of post_title
+  post_title      VARCHAR(255)        NOT NULL  UNIQUE,       -- Title of the post
+  post_slug       VARCHAR(255)        NOT NULL  UNIQUE,       -- URL-safe version of post_title
   post_body       TEXT                NOT NULL,               -- The actual post content
   post_timestamp  DATETIME            DEFAULT NOW()           -- Timestamp of when the post was created (defaults to the moment it's added to the database)
 );
