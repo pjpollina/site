a4fa096cc56963392959c568f33e4f74e9bbd94a
PJ Pollina <contact@pjpollina.info>
2018-11-30 20:02:22 -0500

    Got AJAX actually working

--- public/javascript/post_script.js
+++ public/javascript/post_script.js
@@ -1,8 +1,11 @@
-$("new-post").submit(event => {
-  event.preventDefault();
-  $.ajax({
-    type: "POST",
-    url: "new_blog_post",
-    data: this.serialize
+$(document).ready(function() {
+  $("#new-post").submit(event => {
+    event.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "new_blog_post",
+      data: $("#new-post").serialize()
+    });
   });
+  return 0;
 });
