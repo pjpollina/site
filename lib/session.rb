73fecdc159f2b4f470a4246547d172d3094a4553
PJ Pollina <contact@pjpollina.info>
2018-12-20 14:49:38 -0500

    Added session.rb

---
+++ lib/session.rb
@@ -0,0 +1,29 @@
+# All in one admin session class/singleton thing
+
+require 'digest'
+require 'securerandom'
+
+$admin_session = nil
+
+class Session
+  attr_reader :session_id, :client_ip, :expiration
+
+  def initialize(client_ip)
+    @session_id = Digest::SHA256.hexdigest "#{Time.now}#{SecureRandom.hex(16)}#{client_ip}"
+    @client_ip = client_ip
+    @expiration = Time.now + (60 * 60 * 24 * 7)
+  end
+
+  def expired?
+    Time.now > @expiration
+  end
+
+  def self.set(client_ip)
+    $admin_session = Session.new(client_ip)
+  end
+
+  def self.validate(session_id, client_ip)
+    return false if($admin_session.nil? || $admin_session.expired?)
+    (session_id == $admin_session.session_id) && (client_ip == $admin_session.client_ip)
+  end
+end
