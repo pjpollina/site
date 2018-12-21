155296a841c7d47f814025dff09286bbff14f592
PJ Pollina <contact@pjpollina.info>
2018-12-20 20:58:09 -0500

    Added jQuery event handler for Delete Post button

--- public/javascript/edit_script.js
+++ public/javascript/edit_script.js
@@ -3,9 +3,29 @@ $(document).ready(function() {
     event.preventDefault();
     $.ajax({
       type: "PUT",
+      url: window.location.href.split('?')[0],
       data: $("#edit-post").serialize(),
       success:function(data) {
         document.location = data;
+      },
+      error:function(data) {
+        let errors = JSON.parse(data.responseText);
+        alert(errors);
+      }
+    });
+  });
+
+  $("#delete-post").click(event => {
+    event.preventDefault();
+    if(!confirm("Are you sure?")) {
+      return 0;
+    }
+    $.ajax({
+      type: "DELETE",
+      url: window.location.href.split('?')[0],
+      data: $("#edit-post").serialize(),
+      success:function(data) {
+        document.location = '/';
       },
       error:function(data) {
         let errors = JSON.parse(data.responseText);
