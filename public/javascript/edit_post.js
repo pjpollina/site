42ba432977bae857525b166555a7aed39b1dfc33
PJ Pollina <contact@pjpollina.info>
2019-01-14 10:23:40 -0500

    Tweaked JavaScript syntax

--- public/javascript/edit_post.js
+++ public/javascript/edit_post.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+  function errorCallback(data) {
+    $("main").html(data.responseText);
+  } 
+
   $("#edit-post").submit(event => {
     let url = window.location.href.split('?')[0];
     event.preventDefault();
@@ -6,12 +10,8 @@ $(document).ready(function() {
       type: "PUT",
       url: url,
       data: $("#edit-post").serialize(),
-      success:function(data) {
-        document.location = url;
-      },
-      error:function(data) {
-        $("main").html(data.responseText);
-      }
+      success:() => document.location = url,
+      error:(data) => errorCallback(data)
     });
   });
 
@@ -24,12 +24,8 @@ $(document).ready(function() {
       type: "DELETE",
       url: window.location.href.split('?')[0],
       data: $("#edit-post").serialize(),
-      success:function(data) {
-        document.location = '/';
-      },
-      error:function(data) {
-        $("main").html(data.responseText);
-      }
+      success:() => document.location = '/',
+      error:(data) => errorCallback(data)
     });
   });
 });
