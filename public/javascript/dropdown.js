4d4839007fdfd8d3626e12d2f791c72cee97531c
PJ Pollina <contact@pjpollina.info>
2019-01-15 21:14:09 -0500

    Made the show/hide process of dropdown menus better
    - Changes display property directly instead of adding class .show
    - Removed class .show

--- public/javascript/dropdown.js
+++ public/javascript/dropdown.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $(".navbar-dropdown").mouseenter((event) => {
     let contentId = "#" + event.currentTarget.id + "-content";
-    $(contentId).addClass("show");
+    $(contentId).css({"display": "block"});
   });
 
   $(".navbar-dropdown").mouseleave((event) => {
     let contentId = "#" + event.currentTarget.id + "-content";
-    $(contentId).removeClass("show");
+    $(contentId).css({"display": "none"});
   });
 });
