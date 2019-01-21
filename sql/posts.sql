7d2a16a5e8b23bb1c1efd27c419b54c915b47489
PJ Pollina <contact@pjpollina.info>
2019-01-21 11:52:34 -0500

    Made column post_category a foreign key for the categories table

--- sql/posts.sql
+++ sql/posts.sql
@@ -6,6 +6,7 @@ CREATE TABLE posts (
   post_title      VARCHAR(255)        NOT NULL  UNIQUE,       -- Title of the post
   post_slug       VARCHAR(255)        NOT NULL  UNIQUE,       -- URL-safe version of post_title
   post_body       TEXT                NOT NULL,               -- The actual post content
-  post_category   VARCHAR(255)        DEFAULT "Misc",         -- The post's category
-  post_timestamp  DATETIME            DEFAULT NOW()           -- Timestamp of when the post was created (defaults to the moment it's added to the database)
+  post_category   SMALLINT UNSIGNED   NOT NULL,               -- The post's category
+  post_timestamp  DATETIME            DEFAULT NOW(),          -- Timestamp of when the post was created (defaults to the moment it's added to the database)
+  FOREIGN KEY(post_category) REFERENCES categories(cat_id)
 );
