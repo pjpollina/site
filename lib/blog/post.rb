f1d91990727dc8a5441e24ca800f7919c31773a9
PJ Pollina <contact@pjpollina.info>
2019-01-23 13:21:18 -0500

    Modified namespacing of Blog::Post

--- lib/blog/post.rb
+++ lib/blog/post.rb
@@ -2,24 +2,22 @@
 
 require 'kramdown'
 
-module Website::Blog
-  class Post
-    attr_reader :title, :slug, :body, :category, :timestamp
+module Website::Blog::Post
+  attr_reader :title, :slug, :body, :category, :timestamp
 
-    def initialize(title, slug, body, category, timestamp)
-      @title     = title
-      @slug      = slug
-      @body      = body
-      @category  = category
-      @timestamp = timestamp
-    end
+  def initialize(title, slug, body, category, timestamp)
+    @title     = title
+    @slug      = slug
+    @body      = body
+    @category  = category
+    @timestamp = timestamp
+  end
 
-    def date_formatted
-      @timestamp.strftime("%B %d, %Y")
-    end
+  def date_formatted
+    @timestamp.strftime("%B %d, %Y")
+  end
 
-    def parsed_body
-      Kramdown::Document.new(@body).to_html
-    end
+  def parsed_body
+    Kramdown::Document.new(@body).to_html
   end
 end
