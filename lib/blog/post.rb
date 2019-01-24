4754fd9643516ca1bccb2e2516c85360bd50825d
PJ Pollina <contact@pjpollina.info>
2019-01-24 13:14:04 -0500

    Revert "Modified namespacing of Blog::Post"
    This reverts commit f1d91990727dc8a5441e24ca800f7919c31773a9.

--- lib/blog/post.rb
+++ lib/blog/post.rb
@@ -2,22 +2,24 @@
 
 require 'kramdown'
 
-module Website::Blog::Post
-  attr_reader :title, :slug, :body, :category, :timestamp
+module Website::Blog
+  class Post
+    attr_reader :title, :slug, :body, :category, :timestamp
 
-  def initialize(title, slug, body, category, timestamp)
-    @title     = title
-    @slug      = slug
-    @body      = body
-    @category  = category
-    @timestamp = timestamp
-  end
+    def initialize(title, slug, body, category, timestamp)
+      @title     = title
+      @slug      = slug
+      @body      = body
+      @category  = category
+      @timestamp = timestamp
+    end
 
-  def date_formatted
-    @timestamp.strftime("%B %d, %Y")
-  end
+    def date_formatted
+      @timestamp.strftime("%B %d, %Y")
+    end
 
-  def parsed_body
-    Kramdown::Document.new(@body).to_html
+    def parsed_body
+      Kramdown::Document.new(@body).to_html
+    end
   end
 end
