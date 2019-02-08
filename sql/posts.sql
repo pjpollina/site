b121fe9302000dc314eddd528b9faf4444da1482
PJ Pollina <contact@pjpollina.info>
2019-02-08 18:49:44 -0500

    Made post_id auto incrementing

--- sql/posts.sql
+++ sql/posts.sql
@@ -2,10 +2,10 @@
 -- Formatted for MySQL
 
 CREATE TABLE posts (
-  post_id         SMALLINT UNSIGNED   NOT NULL  PRIMARY KEY,  -- The primary key
-  post_title      VARCHAR(255)        NOT NULL  UNIQUE,       -- Title of the post
-  post_slug       VARCHAR(255)        NOT NULL  UNIQUE,       -- URL-safe version of post_title
-  post_body       TEXT                NOT NULL,               -- The actual post content
-  post_category   VARCHAR(255)        NOT NULL,               -- The post's category
-  post_timestamp  DATETIME            DEFAULT NOW(),          -- Timestamp of when the post was created (defaults to the moment it's added to the database)
+  post_id         SMALLINT UNSIGNED   PRIMARY KEY AUTO_INCREMENT, -- The primary key
+  post_title      VARCHAR(255)        NOT NULL  UNIQUE,           -- Title of the post
+  post_slug       VARCHAR(255)        NOT NULL  UNIQUE,           -- URL-safe version of post_title
+  post_body       TEXT                NOT NULL,                   -- The actual post content
+  post_category   VARCHAR(255)        NOT NULL,                   -- The post's category
+  post_timestamp  DATETIME            DEFAULT NOW()               -- Timestamp of when the post was created (defaults to the moment it's added to the database)
 );
