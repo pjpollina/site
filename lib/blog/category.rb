3fac64ea73e4afb79ba29d0c6729a7a636ddeb2f
PJ Pollina <contact@pjpollina.info>
2019-02-08 18:24:37 -0500

    Switched Category namespacing to Ruby standard

--- lib/blog/category.rb
+++ lib/blog/category.rb
@@ -1,21 +1,25 @@
 # Class for a blog category object
 
-class Website::Blog::Category
-  attr_reader :name, :slug, :desc, :posts
+module Website
+  module Blog
+    class Category
+      attr_reader :name, :slug, :desc, :posts
 
-  def initialize(name, slug, desc, posts)
-    @name  = name
-    @slug  = slug
-    @desc  = desc
-    @posts = posts
-  end
+      def initialize(name, slug, desc, posts)
+        @name  = name
+        @slug  = slug
+        @desc  = desc
+        @posts = posts
+      end
 
-  def self.name_to_slug(name)
-    name.downcase.gsub(' ', '_')
-  end
+      def self.name_to_slug(name)
+        name.downcase.gsub(' ', '_')
+      end
 
-  def self.slug_to_name(slug)
-    words = slug.split('_').collect {|word| word.capitalize }
-    words.join(' ')
+      def self.slug_to_name(slug)
+        words = slug.split('_').collect {|word| word.capitalize }
+        words.join(' ')
+      end
+    end
   end
 end
