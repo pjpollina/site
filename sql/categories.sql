d9a03468157b2063838ed56c6ae833ac45078a53
PJ Pollina <contact@pjpollina.info>
2019-02-08 18:42:53 -0500

    Removed unnecessary columns cat_id and cat_slug

--- sql/categories.sql
+++ sql/categories.sql
@@ -2,8 +2,6 @@
 -- Formatted for MySQL
 
 CREATE TABLE categories (
-  cat_id    SMALLINT UNSIGNED  NOT NULL PRIMARY KEY,                    -- The primary key
-  cat_name  VARCHAR(255)       NOT NULL UNIQUE,                         -- The name of the category
-  cat_slug  VARCHAR(255)       AS (LOWER(REPLACE(cat_name, " ", "_"))), -- The slug for the category, automatically derived from the name
-  cat_desc  VARCHAR(255)                                                -- The description of the category
+  cat_name  VARCHAR(255) NOT NULL PRIMARY KEY, -- The name of the category
+  cat_desc  VARCHAR(255)                       -- The description of the category
 );
