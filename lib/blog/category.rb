6880bc0c5b9bbc5ac94c9bf83879c949b37e3bd1
PJ Pollina <contact@pjpollina.info>
2019-02-09 17:48:27 -0500

    Removed member variable @slug

--- lib/blog/category.rb
+++ lib/blog/category.rb
@@ -3,11 +3,10 @@
 module Website
   module Blog
     class Category
-      attr_reader :name, :slug, :desc, :posts
+      attr_reader :name, :desc, :posts
 
-      def initialize(name, slug, desc, posts)
+      def initialize(name, desc, posts)
         @name  = name
-        @slug  = slug
         @desc  = desc
         @posts = posts
       end
