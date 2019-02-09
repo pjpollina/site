064063ccefd06d5c71a7e71572f4b3f5eea41d4b
PJ Pollina <contact@pjpollina.info>
2019-02-09 18:08:54 -0500

    Slight organizational tweaks

--- lib/blog/database.rb
+++ lib/blog/database.rb
@@ -12,19 +12,19 @@ module Website
         @insert = @sql_client.prepare "INSERT INTO posts(post_title, post_slug, post_body, post_category) VALUES(?, ?, ?, ?)"
         @update = @sql_client.prepare "UPDATE posts SET post_body=? WHERE post_slug=?"
         @delete = @sql_client.prepare "DELETE FROM posts WHERE post_slug=?"
-        # Info getters
+        # Info checkers
         @title_free = @sql_client.prepare "SELECT EXISTS(SELECT * FROM posts WHERE post_title=?) AS used"
         @slug_free  = @sql_client.prepare "SELECT EXISTS(SELECT * FROM posts WHERE post_slug =?) AS used"
         # Post getters
-        @get_post     = @sql_client.prepare "SELECT * FROM posts WHERE post_slug=?"
-        @recent_posts = @sql_client.prepare "SELECT * FROM posts ORDER BY post_timestamp DESC LIMIT ?"
+        @get_post       = @sql_client.prepare "SELECT * FROM posts WHERE post_slug=?"
+        @recent_posts   = @sql_client.prepare "SELECT * FROM posts ORDER BY post_timestamp DESC LIMIT ?"
+        @category_posts = @sql_client.prepare "SELECT * FROM posts WHERE post_category=? ORDER BY post_timestamp"
         # Category functions
         @categories     = @sql_client.prepare "SELECT cat_name FROM categories"
         @get_category   = @sql_client.prepare "SELECT cat_name, cat_desc FROM categories WHERE cat_name=?"
-        @category_posts = @sql_client.prepare "SELECT * FROM posts WHERE post_category=? ORDER BY post_timestamp"
       end
 
-      # Post modifiers
+      # Post Insert/Update/Delete statements
       def insert(title, slug, body, category)
         @insert.execute(title, slug, body, category)
         unless(categories.include?(category))
