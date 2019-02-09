c037fa07f719a99ee940b2a49bf25285d8ffe0b7
PJ Pollina <contact@pjpollina.info>
2019-02-09 18:10:44 -0500

    Updated references to etc directory

--- lib/website.rb
+++ lib/website.rb
@@ -1,8 +1,8 @@
 # Global hash of configuration settings
-# Loads file ./etc/config.yaml
+# Loads file ./data/config.yaml
 
 require 'yaml'
 
 module Website
-  $config_info = Hash[YAML.load(File.read('./etc/config.yaml')).map {|k, v| [k.to_sym, v]}]
+  $config_info = Hash[YAML.load(File.read('./data/config.yaml')).map {|k, v| [k.to_sym, v]}]
 end
