871d2fe08149bee48cc310db5b06f864a4af13fb
PJ Pollina <contact@pjpollina.info>
2019-02-09 17:50:37 -0500

    Added post_preview column

--- sql/posts.sql
+++ sql/posts.sql
@@ -7,5 +7,6 @@ CREATE TABLE posts (
   post_slug       VARCHAR(255)        NOT NULL  UNIQUE,           -- URL-safe version of post_title
   post_body       TEXT                NOT NULL,                   -- The actual post content
   post_category   VARCHAR(255)        NOT NULL,                   -- The post's category
-  post_timestamp  DATETIME            DEFAULT NOW()               -- Timestamp of when the post was created (defaults to the moment it's added to the database)
+  post_timestamp  DATETIME            DEFAULT NOW(),              -- Timestamp of when the post was created (defaults to the moment it's added to the database)
+  post_preview    TEXT                AS(SUBSTRING_INDEX(post_body, '\r\n', 1))
 );
