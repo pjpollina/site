b8c560199352f2a0cd82c53446ef24a0e9457694
PJ Pollina <contact@pjpollina.info>
2019-02-10 14:22:29 -0500

    Removed page_info function

--- lib/page_builder.rb
+++ lib/page_builder.rb
@@ -9,13 +9,6 @@ module Website
     VIEWS   = HTTPServer.web_file("templates/views")
     LAYOUTS = HTTPServer.web_file("templates/layouts")
 
-    def self.page_info(page_name, admin)
-      info = OpenStruct.new
-      info.page_name = page_name
-      info.admin = admin
-      return info
-    end
-
     def self.load_view(name)
       Tilt::ERBTemplate.new("#{VIEWS}/#{name}")
     end
@@ -30,7 +23,7 @@ module Website
       end
 
       def [](page_name, admin)
-        PageBuilder.load_layout(@name).render(PageBuilder.page_info(page_name, admin)) do
+        PageBuilder.load_layout(@name).render(OpenStruct.new(page_name: page_name, admin: admin)) do
           yield
         end
       end
