8d062ef94223ac086eb5dbefa6371325896372e9
PJ Pollina <contact@pjpollina.info>
2019-02-11 12:44:24 -0500

    Added blog/renderer.rb

---
+++ lib/blog/renderer.rb
@@ -0,0 +1,55 @@
+require './lib/page_builder.rb'
+
+module Website
+  module Blog
+    module Renderer
+      LAYOUT = PageBuilder::Layout.new('layout.erb')
+
+      VIEWS = {
+        homepage:  PageBuilder::View.new('homepage.erb'),
+        archive:   PageBuilder::View.new('archive.erb'),
+        new_post:  PageBuilder::View.new('new_post.erb'),
+        post:      PageBuilder::View.new('post.erb'),
+        edit_post: PageBuilder::View.new('edit_post.erb'),
+        category:  PageBuilder::View.new('category.erb')
+      }
+
+      # Page Renderers
+      def self.render_page(name, view, admin, admin_locked, locals)
+        if(admin_locked && !admin)
+          render_403
+        else
+          page = LAYOUT[name, admin] do
+            VIEWS[view][locals]
+          end
+          HTTPServer.html_response(page)
+        end
+      end
+
+      def self.render_post_page(slug, edit=false)
+        post = @database.get_post(slug)
+        if(post.nil?)
+          render_404
+        else
+          name, view = ((edit) ? ["Editing Post #{post.title}", :edit_post] : [post.title, :post])
+          render_page(name, view, edit, post: post, admin: admin)
+        end
+      end
+
+      def self.render_403(admin, simple=false)
+        page = File.read HTTPServer.web_file("403.html")
+        unless(simple)
+          page = LAYOUT["403", admin] { page }
+        end
+        HTTPServer.html_response(page, 403, 'Forbidden')
+      end
+
+      def self.render_404(admin)
+        page = LAYOUT["404", admin] do
+          File.read HTTPServer.web_file("404.html")
+        end
+        HTTPServer.html_response(page, 404, 'Not Found')
+      end
+    end
+  end
+end
