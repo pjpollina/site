55c92ab63218e02b028f7d643d3f94ea96eba218
PJ Pollina <contact@pjpollina.info>
2019-02-21 08:34:42 -0500

    Changed qualified name to Website::HTTP::Server

--- lib/website/http/server.rb
+++ lib/website/http/server.rb
@@ -7,30 +7,32 @@ require 'website/http/request'
 require 'website/admin_session'
 
 module Website
-  class HTTPServer
-    def initialize(hostname: $config_info[:host_name], port: $config_info[:port])
-      @tcp = TCPServer.new(hostname, port)
-      @ssl = OpenSSL::SSL::SSLServer.new(@tcp, ssl_context)
-    end
-
-    def serve(https: false)
-      begin
-        socket = (https) ? @ssl.accept : @tcp.accept
-        request = HTTP::Request[socket]
-        yield(socket, request)
-        socket.close
-      rescue OpenSSL::SSL::SSLError => error
-        STDERR.puts("SSL Error: #{error.message}")
+  module HTTP
+    class Server
+      def initialize(hostname: $config_info[:host_name], port: $config_info[:port])
+        @tcp = TCPServer.new(hostname, port)
+        @ssl = OpenSSL::SSL::SSLServer.new(@tcp, ssl_context)
       end
-    end
 
-    private
+      def serve(https: false)
+        begin
+          socket = (https) ? @ssl.accept : @tcp.accept
+          request = HTTP::Request[socket]
+          yield(socket, request)
+          socket.close
+        rescue OpenSSL::SSL::SSLError => error
+          STDERR.puts("SSL Error: #{error.message}")
+        end
+      end
 
-    def ssl_context
-      ssl_context = OpenSSL::SSL::SSLContext.new
-      ssl_context.ssl_version = :SSLv23
-      ssl_context.add_certificate(OpenSSL::X509::Certificate.new(File.open(ENV['blogapp_ssl_cert'])), OpenSSL::PKey::RSA.new(File.open(ENV['blogapp_ssl_key'])))
-      return ssl_context
+      private
+
+      def ssl_context
+        ssl_context = OpenSSL::SSL::SSLContext.new
+        ssl_context.ssl_version = :SSLv23
+        ssl_context.add_certificate(OpenSSL::X509::Certificate.new(File.open(ENV['blogapp_ssl_cert'])), OpenSSL::PKey::RSA.new(File.open(ENV['blogapp_ssl_key'])))
+        return ssl_context
+      end
     end
   end
 end
