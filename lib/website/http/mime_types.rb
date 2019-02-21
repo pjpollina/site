fd2e0df708c197d77c2abd6ed76c8d18ea96f2e3
PJ Pollina <contact@pjpollina.info>
2019-02-21 08:04:06 -0500

    Added http/mime_types.rb

---
+++ lib/website/http/mime_types.rb
@@ -0,0 +1,15 @@
+# "Enum" of supported MIME types
+
+module Website
+  module HTTP
+    MIME_TYPES = {
+      'css'  => 'text/css',
+      'png'  => 'image/png',
+      'jpg'  => 'image/jpeg',
+      'ico'  => 'image/x-icon',
+      'json' => 'application/json',
+      'js'   => 'application/javascript',
+      'jsx'  => 'application/javascript'
+    }
+  end
+end
