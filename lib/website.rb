6834f2634c74645767ae98fa21ac0b425cc7d6e6
PJ Pollina <contact@pjpollina.info>
2019-02-28 17:03:03 -0500

    Removed stuff that was moved to Utils

--- lib/website.rb
+++ lib/website.rb
@@ -1,23 +1,7 @@
 # Global website namespace
 
-require 'uri'
 require 'yaml'
 
 module Website
-  WEB_ROOT = File.expand_path(File.dirname(__FILE__)).gsub('lib', 'public/')
-
-  def self.web_file(path)
-    WEB_ROOT + path
-  end
-
-  def self.parse_form_data(form_data)
-    elements = {}
-    form_data.split('&').each do |element| 
-      key, value = element.split('=', 2)
-      elements[key.to_sym] = URI.decode(value).gsub('+', ' ')
-    end
-    elements
-  end
-
   $config_info = Hash[YAML.load(File.read(File.expand_path(File.dirname(__FILE__)).gsub('lib', 'data/config.yaml'))).map {|k, v| [k.to_sym, v]}]
 end
