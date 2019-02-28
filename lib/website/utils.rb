84a6f6e367e3ebdd7d286e6d236182ac9d1ab605
PJ Pollina <contact@pjpollina.info>
2019-02-28 16:52:01 -0500

    Added utils.rb

---
+++ lib/website/utils.rb
@@ -0,0 +1,22 @@
+# Helper module for functions that don't fit in anywhere else
+
+module Website
+  module Utils
+    extend self
+
+    WEB_ROOT = File.expand_path(File.dirname(__FILE__)).gsub('lib/website', 'public/')
+
+    def web_file(path)
+      WEB_ROOT + path
+    end
+
+    def parse_form_data(form_data)
+      elements = {}
+      form_data.split('&').each do |element| 
+        key, value = element.split('=', 2)
+        elements[key.to_sym] = URI.decode(value).gsub('+', ' ')
+      end
+      return elements
+    end
+  end
+end
