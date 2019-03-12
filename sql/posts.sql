b0e63d352b5ca1ff97f7b14493f729d9a86bf3f5
PJ Pollina <contact@pjpollina.info>
2019-03-12 17:18:45 -0400

    Added column post_desc

--- sql/posts.sql
+++ sql/posts.sql
@@ -5,6 +5,7 @@ CREATE TABLE posts (
   post_id         SMALLINT UNSIGNED   PRIMARY KEY AUTO_INCREMENT, -- The primary key
   post_title      VARCHAR(255)        NOT NULL  UNIQUE,           -- Title of the post
   post_slug       VARCHAR(255)        NOT NULL  UNIQUE,           -- URL-safe version of post_title
+  post_desc       VARCHAR(255),                                   -- A short description of the post
   post_body       TEXT                NOT NULL,                   -- The actual post content
   post_category   VARCHAR(255)        NOT NULL,                   -- The post's category
   post_timestamp  DATETIME            DEFAULT NOW(),              -- Timestamp of when the post was created (defaults to the moment it's added to the database)
