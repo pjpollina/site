c06400197509e6b0e5cd30bbf11a9a839beb4673
PJ Pollina <contact@pjpollina.info>
2019-04-07 16:29:06 -0400

    Integrated WebFile into RSS

--- lib/website/blog/rss.rb
+++ lib/website/blog/rss.rb
@@ -1,9 +1,11 @@
 # Class for blog's RSS feed
 
+require 'website/web_file'
+
 module Website
   module Blog
     class RSS
-      TEMPLATE = Tilt::ERBTemplate.new(Utils.web_file('templates/feed.erb'))
+      TEMPLATE = Tilt::ERBTemplate.new(WebFile.expand_path('templates/feed.erb'))
 
       def initialize(post_count: 5, feed_path: 'feed.rss')
         @post_count = post_count
@@ -11,7 +13,7 @@ module Website
       end
 
       def update(database)
-        File.write(Utils.web_file(@feed_path), TEMPLATE.render(nil, posts: database.recent_posts(@post_count)))
+        WebFile.write(@feed_path, TEMPLATE.render(nil, posts: database.recent_posts(@post_count)))
       end
     end
   end
