23f15e593fd5066fe8e5e0d57da62ae12190096a
PJ Pollina <contact@pjpollina.info>
2019-04-10 23:38:04 -0400

    Renamed function full_uri to full_url

--- lib/website/utils.rb
+++ lib/website/utils.rb
@@ -13,7 +13,7 @@ module Website
       return elements
     end
 
-    def full_uri
+    def full_url
       path = 'https://' <<  Website.config_info[:host_name]
       unless([443, 80].include?(Website.config_info[:port]))
         path << ":#{Website.config_info[:port]}"
