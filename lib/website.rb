4c0c8b042efe222fbab64b904bf3ae05c919c578
PJ Pollina <contact@pjpollina.info>
2019-04-13 18:32:36 -0400

    Added data_file function

--- lib/website.rb
+++ lib/website.rb
@@ -5,6 +5,10 @@ require 'yaml'
 module Website
   extend self
 
+  def data_file(path)
+    File.expand_path(File.dirname(__FILE__)).gsub('lib', 'data') + ((path.start_with?('/')) ? '' : '/') + path
+  end
+
   def config_info
     @config_info ||= begin
       hash = Hash.new("")
