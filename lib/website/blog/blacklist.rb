3fccc27b55ba68bbf6f80b91c692a78a94595350
PJ Pollina <contact@pjpollina.info>
2019-04-13 18:53:39 -0400

    Added blog/blacklist.rb

---
+++ lib/website/blog/blacklist.rb
@@ -0,0 +1,40 @@
+module Website
+  module Blog
+    class Blacklist
+      def initialize(filename=".blacklist")
+        @blacklist = Website.data_file(filename)
+        @ip_login_attempts = Hash.new(0)
+        load_blacklist
+        puts @ip_login_attempts
+      end
+
+      def banned?(ip)
+        @ip_login_attempts[ip] >= 3
+      end
+
+      def add_attempt(ip)
+        @ip_login_attempts[ip] += 1
+      end
+
+      def clear_attempts(ip)
+        @ip_login_attempts[ip] = 0
+      end
+
+      def blacklist_ip(ip)
+        File.open(@blacklist, "a") do |file|
+          file.puts(ip)
+        end
+      end
+
+      private
+
+      def load_blacklist
+        if(File.exist?(@blacklist) && !File.directory?(@blacklist))
+          File.open(@blacklist) do |file|
+            file.each { |ip| @ip_login_attempts[ip.chomp] = 3 }
+          end
+        end
+      end
+    end
+  end
+end
