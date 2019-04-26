21e2a66b03db7d5fd7cc6204813ee8848709d606
PJ Pollina <contact@pjpollina.info>
2019-04-26 15:53:22 -0400

    Added form_validator.js

---
+++ public/javascript/form_validator.js
@@ -0,0 +1,40 @@
+function validate(element) {
+  let input = element.value;
+  let error = document.getElementById(element.id + "-error");
+  error.innerHTML = "";
+  if(element.required && !input.length) {
+    error.style.display = "flex";
+    error.innerHTML = "Field cannot be blank";
+  }
+  if(element.hasAttribute("unique") && input.length) {
+    let ajax = new XMLHttpRequest();
+    ajax.open("GET", "/validate?" + element.id + "=" + encodeURI(input), true);
+    ajax.onload = function() {
+      if(ajax.responseText === 'false') {
+        error.style.display = "flex";
+        error.innerHTML = "Requested input already in use";
+      }
+    }
+    ajax.send();
+  }
+  if(element.hasAttribute("regexp") && input.length) {
+    let regexp = new RegExp(element.getAttribute("regexp"));
+    if(!regexp.test(input)) {
+      error.style.display = "flex";
+      error.innerHTML = "Illegal formatting";
+    }
+  }
+  submitCheck();
+}
+
+function submitCheck() {
+	let submit = document.getElementById("submit");
+	let fields = document.getElementsByClassName("error");
+  for(let i = 0; i < fields.length; i++) {
+  	if(fields[i].innerHTML.length) {
+    	submit.disabled = true;
+      return;
+    }
+  }
+  submit.disabled = false;
+}
