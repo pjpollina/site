d1e1792d13fa9dd19924aeff1cdebb330c82ae6d
PJ Pollina <contact@pjpollina.info>
2019-04-26 16:01:04 -0400

    Removed jQuery validator stuff

--- public/javascript/new_post.js
+++ public/javascript/new_post.js
@@ -1,47 +1,9 @@
 $(document).ready(function() {
-  init_validator();
   slug_autocomplete();
   handle_body_upload();
   submit_new_post();
   return 0;
 });
-
-function init_validator() {
-  $.validator.addMethod("regex", function(value, element, regex) {
-    return this.optional(element) || new RegExp(regex).test(value);
-  });
-
-  $("#new-post").validate({
-    errorClass: "error",
-    onkeyup: false,
-    rules: {
-      title: {
-        remote: "/validate"
-      },
-      slug: {
-        regex: /^[A-Za-z0-9]+(?:[A-Za-z0-9_-]+[A-Za-z0-9]){0,255}$/,
-        remote: "/validate"
-      }
-    },
-    messages: {
-      title: {
-        remote: "Title already in use!"
-      },
-      slug: {
-        regex: "Slug is invalid!",
-        remote: "Slug already in use!"
-      }
-    }
-  });
-
-  $("#new-post").bind('blur click', () => {
-    if($("#new-post").validate().checkForm()) {
-      $("#submit").prop('disabled', false);
-    } else {
-      $("#submit").prop('disabled', true);
-    }
-  });
-}
 
 function slug_autocomplete() {
   if(this.slug_edited === undefined) {
