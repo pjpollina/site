836b200cee8250b5ce9ee93ee4891f18d7ac3fd5
PJ Pollina <contact@pjpollina.info>
2019-04-29 14:54:06 -0400

    De-jQueryed the post delete button

--- public/javascript/edit_post.js
+++ public/javascript/edit_post.js
@@ -3,19 +3,7 @@ $(document).ready(function() {
     $("main").html(data.responseText);
   }
   sendPostUpdate();
-  $("#delete-post").click(event => {
-    event.preventDefault();
-    if(!confirm("Are you sure?")) {
-      return 0;
-    }
-    $.ajax({
-      type: "DELETE",
-      url: window.location.href.split('?')[0],
-      data: $("#edit-post").serialize(),
-      success:() => document.location = '/',
-      error:(data) => errorCallback(data)
-    });
-  });
+  sendPostDelete();
 });
 
 function sendPostUpdate() {
@@ -29,6 +17,31 @@ function sendPostUpdate() {
       switch(ajax.status) {
         case 200:
           document.location = url;
+          break;
+        default:
+          alert("An unknown error has occured");
+          break;
+      }
+    };
+    ajax.send(new URLSearchParams(new FormData(form)).toString());
+  });
+}
+
+function sendPostDelete() {
+  let url = window.location.href.split('?')[0];
+  let form = document.getElementById("edit-post");
+  let deleter = document.getElementById("delete-post");
+  deleter.addEventListener("click", event => {
+    event.preventDefault();
+    if(!confirm("Are you sure?")) {
+      return;
+    }
+    let ajax = new XMLHttpRequest();
+    ajax.open("DELETE", url, true);
+    ajax.onload = function() {
+      switch(ajax.status) {
+        case 200:
+          document.location = "/";
           break;
         default:
           alert("An unknown error has occured");
