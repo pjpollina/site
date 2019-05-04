e9925b21bf2a206b7df9b1a9e7b68eacf47a8160
PJ Pollina <contact@pjpollina.info>
2019-05-04 18:47:18 -0400

    Changed qualified name to Website::Admin::Session

--- lib/website/admin/session.rb
+++ lib/website/admin/session.rb
@@ -5,62 +5,64 @@ require 'digest'
 require 'securerandom'
 
 module Website
-  class AdminSession
-    attr_reader :session_id, :client_ip, :expiration
+  module Admin
+    class Session
+      attr_reader :session_id, :client_ip, :expiration
 
-    def initialize(client_ip)
-      @session_id = Digest::SHA256.hexdigest "#{Time.now}#{SecureRandom.hex(16)}#{client_ip}"
-      @client_ip = client_ip
-      @expiration = Time.now + (60 * 60 * 24 * 7)
-    end
-
-    def expired?
-      Time.now > @expiration
-    end
-
-    class << self
-      @session = nil
-
-      def set(client_ip)
-        @session = new(client_ip)
+      def initialize(client_ip)
+        @session_id = Digest::SHA256.hexdigest "#{Time.now}#{SecureRandom.hex(16)}#{client_ip}"
+        @client_ip = client_ip
+        @expiration = Time.now + (60 * 60 * 24 * 7)
       end
 
-      def validate(session_id, client_ip)
-        return false if(@session.nil? || @session.expired?)
-        (session_id == @session.session_id) && (client_ip == @session.client_ip)
+      def expired?
+        Time.now > @expiration
       end
 
-      def cookie
-        "session_id=#{@session.session_id}; Expires=#{@session.expiration.httpdate}; HttpOnly"
-      end
-
-      def login_request(client_ip, redirect='/')
-        AdminSession.set(client_ip)
-        <<~HEREDOC
-          HTTP/1.1 200 OK\r
-          Set-Cookie: #{AdminSession.cookie}\r
-          \r
-          #{redirect}
-        HEREDOC
-      end
-
-      def logout_request
-        mesg = 'Logout successful'
-        <<~HEREDOC
-          HTTP/1.1 200 OK\r
-          Content-Type: text/html
-          Content-Length: #{mesg.bytesize}
-          Set-Cookie: session_id=; Expires=#{Time.now.httpdate}; HttpOnly\r
-          \r
-          #{mesg}
-        HEREDOC
-      end
-
-      def unset
+      class << self
         @session = nil
-      end
 
-      private :new
+        def set(client_ip)
+          @session = new(client_ip)
+        end
+
+        def validate(session_id, client_ip)
+          return false if(@session.nil? || @session.expired?)
+          (session_id == @session.session_id) && (client_ip == @session.client_ip)
+        end
+
+        def cookie
+          "session_id=#{@session.session_id}; Expires=#{@session.expiration.httpdate}; HttpOnly"
+        end
+
+        def login_request(client_ip, redirect='/')
+          Session.set(client_ip)
+          <<~HEREDOC
+            HTTP/1.1 200 OK\r
+            Set-Cookie: #{Session.cookie}\r
+            \r
+            #{redirect}
+          HEREDOC
+        end
+
+        def logout_request
+          mesg = 'Logout successful'
+          <<~HEREDOC
+            HTTP/1.1 200 OK\r
+            Content-Type: text/html
+            Content-Length: #{mesg.bytesize}
+            Set-Cookie: session_id=; Expires=#{Time.now.httpdate}; HttpOnly\r
+            \r
+            #{mesg}
+          HEREDOC
+        end
+
+        def unset
+          @session = nil
+        end
+
+        private :new
+      end
     end
   end
 end
